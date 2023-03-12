beed7b917ff6a182bf54ea5e22deee104333ad0e
StasBabina <ebssmath@gmail.com>
2023-03-12 19:23:36 +0300

    First Commit.

---
+++ src/main/java/com/mvcapp/pizzaforyou/PizzaForYouApplication.java
@@ -0,0 +1,13 @@
+package com.mvcapp.pizzaforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaForYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaForYouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mvcapp/pizzaforyou/PizzaForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvcapp.pizzaforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaForYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
